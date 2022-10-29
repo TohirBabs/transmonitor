d7bb174d13b434f42b9dc750b775069b37244474
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2022-10-29 12:53:33 +0100

    enabling responsiveness

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,14 +1,28 @@
+import { useState, useEffect } from "react";
+import { MobileNavbar } from "../Components/navbar/MobileNavbar";
+
 import Navbar from "../Components/navbar/Navbar";
 import Overview from "../Components/overview/Overview";
 import Sidebar from "../Components/sidebar/Sidebar";
 import "./dashboard.scss";
 
 const Dashboard = () => {
+  const [mobileView, setmobileView] = useState(false);
+  var width;
+
+  function handleWindowSizeChange(params) {
+    setmobileView(window.innerWidth <= 768);
+    console.log(window.innerWidth);
+  }
+  useEffect(() => {
+    handleWindowSizeChange();
+  }, []);
+
   return (
     <div className="dashboard">
-      <Navbar />
+      {mobileView ? <MobileNavbar /> : <Navbar />}
       <div className="dashboard_layout">
-        <Sidebar />
+        {mobileView ? <></> : <Sidebar />}
         <Overview />
       </div>
     </div>
