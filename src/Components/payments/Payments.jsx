f5ca8c4b6d24e5333d252718d2ec25526fc39fd9
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2022-10-31 00:38:22 +0100

    sync category selection with payment list

--- src/Components/payments/Payments.jsx
+++ src/Components/payments/Payments.jsx
@@ -3,8 +3,10 @@ import { SearchIconsm } from "../icons/SearchIconsm";
 
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import "./payment.scss";
+import { useState } from "react";
 
 export const Payments = () => {
+  const [activePage, setactivePage] = useState(1);
   const paymentStatus = [
     "Reconciled",
     "Pending",
@@ -17,6 +19,8 @@ export const Payments = () => {
     "Pending",
   ];
 
+  const [paymentSelect, setpaymentSelect] = useState("All");
+
   const PaymentRow = ({ status }) => {
     return (
       <div className="payment_row">
@@ -27,10 +31,49 @@ export const Payments = () => {
         <div className="price">$73430</div>
         <div className="transaction_no">1234567890</div>
         <div className="time">12:30</div>
-        <div className={`status ${status.toLowerCase()}`}>
-          <div className="dot"></div>
-          {status}
+        <div className="stat">
+          <div className={`status ${status.toLowerCase()}`}>
+            <div className="dot"></div>
+            {status}
+          </div>
+          <KeyboardArrowDownIcon />
         </div>
+      </div>
+    );
+  };
+
+  const PaymentSelection = () => {
+    const [openSelection, setopenSelection] = useState(false);
+
+    const selections = [
+      "All",
+      "Reconciled",
+      "Un-reconciled",
+      "Settled",
+      "Un-settled",
+    ];
+
+    return (
+      <div className="payment_selection">
+        <p>Show</p>
+        <div
+          className="select"
+          onClick={() => setopenSelection(!openSelection)}
+        >
+          <p>{paymentSelect}</p>
+          <KeyboardArrowDownIcon color="primary" />
+        </div>
+        <ul className={`selections ${openSelection ? "open" : "close"}`}>
+          {selections.map((selection) => (
+            <li
+              onClick={() => {
+                setpaymentSelect(selection);
+              }}
+            >
+              {selection}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   };
@@ -41,7 +84,7 @@ export const Payments = () => {
       <div className="payment_info">
         <div className="right">
           <p className="showing">
-            showing{" "}
+            Showing{" "}
             <span className="amt_shown">
               <span>20</span> <KeyboardArrowDownIcon />
             </span>
@@ -55,20 +98,7 @@ export const Payments = () => {
             <input type="text" placeholder="Search payments" />
           </div>
         </div>
-        <div className="left">
-          <p>Show</p>
-          <div className="select">
-            <p>All</p>
-            <KeyboardArrowDownIcon color="primary" />
-          </div>
-          <ul className="selections">
-            <li>All</li>
-            <li>Reconciled</li>
-            <li>Un-reconciled</li>
-            <li>Settled</li>
-            <li>Un-settled</li>
-          </ul>
-        </div>
+        <PaymentSelection />
       </div>
       <div className="payment_table">
         <div className="header">
@@ -78,18 +108,35 @@ export const Payments = () => {
           <p>Time</p>
           <p>Status </p>
         </div>
-        {paymentStatus.map((payment) => (
-          <PaymentRow status={payment} />
-        ))}
+
+        {paymentSelect === "All"
+          ? paymentStatus.map((payment) => <PaymentRow status={payment} />)
+          : paymentStatus
+              .filter((pay) => pay === paymentSelect)
+              .map((payment) => <PaymentRow status={payment} />)}
       </div>
 
       <div className="bottom">
         <p>Showing 1 to 10 of 500 entries</p>
         <div className="pagination">
-          <div className="prev">Previous</div>
-          <div className="page active">1</div>
-          <div className="page">2</div>
-          <div className="next">Next</div>
+          <div
+            onClick={() => {
+              setactivePage(1);
+            }}
+            className="prev"
+          >
+            Previous
+          </div>
+          <div className={`page ${activePage == 1 ? "active" : ""}`}>1</div>
+          <div className={`page ${activePage == 2 ? "active" : ""}`}>2</div>
+          <div
+            onClick={() => {
+              setactivePage(2);
+            }}
+            className="next"
+          >
+            Next
+          </div>
         </div>
       </div>
     </div>
