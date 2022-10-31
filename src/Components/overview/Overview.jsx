5ffd631df4457bfbcaa1e5659f6c27579e9b4eba
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2022-10-31 16:39:17 +0100

    adding box shadows on hover

--- src/Components/overview/Overview.jsx
+++ src/Components/overview/Overview.jsx
@@ -8,9 +8,7 @@ const Overview = () => {
   const InfoCards = [
     { name: "Daily Transaction Volume", value: "2,342" },
     { name: "Daily Transaction Value", value: "₦4,000,000" },
-
     { name: "Total Transaction Volume", value: "452,000" },
-
     { name: "Total Transaction Value", value: "₦4,000,000" },
   ];
   return (
